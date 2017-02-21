f97a2246fb7c941f3be412882e40139250dea855
ChrisFrenzel <ChrisFrenzel@users.noreply.github.com>
2017-02-21 11:54:12 +0100

    initial maven archetype

---
+++ src/main/java/de/isse/alumni/muenchner_kindl_buam/App.java
@@ -0,0 +1,13 @@
+package de.isse.alumni.muenchner_kindl_buam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
