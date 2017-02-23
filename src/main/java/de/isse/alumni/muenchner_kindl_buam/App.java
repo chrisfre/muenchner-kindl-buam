ca41b47e393194e8c76200d618a28539d0420629
Adrian Rumpold <a.rumpold@gmail.com>
2017-02-23 14:05:48 +0100

    Merge main app ;)

--- src/main/java/de/isse/alumni/muenchner_kindl_buam/App.java
+++ src/main/java/de/isse/alumni/muenchner_kindl_buam/App.java
@@ -1,5 +1,11 @@
 package de.isse.alumni.muenchner_kindl_buam;
 
+import org.jooq.lambda.Seq;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
 /**
  * Hello world!
  *
@@ -8,8 +14,23 @@ public class App
 {
     public static void main( String[] args )
     {
-    	LombokHelloWorld lhw = new LombokHelloWorld("Arjen", 12);
-    	
-        System.out.println( "Hello World! " + lhw.getAge() );
+        System.out.println( "Hello World!" );
+        
+        // Lombok features
+        final Test test = Test.builder().value(42).build();
+        System.out.println(test);
+        
+        // jOOL features, tests Java 8 language compat
+        final Seq<Integer> nums = Seq.of(1, 2, 3);
+        final Seq<Character> chars = Seq.of('a', 'b');
+        nums.crossJoin(chars)
+            .forEach(t -> System.out.println(t));
     }
+}
+
+@Builder
+@ToString
+class Test {
+    @Getter
+    private final int value;
 }
